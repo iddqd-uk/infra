bef33f9aa769ad371e2cda1f55eba33858697305
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2025-01-30 00:05:03 +0400

    remove deprecated host

--- dns/dnsconfig.js
+++ dns/dnsconfig.js
@@ -1,8 +1,7 @@
 // @ts-check
 /// <reference path=".types.d.ts" />
 
-var legacyIPv4 = '95.217.4.242', legacyIPv6 = '2a01:4f9:c011:4ae2::1';
-var kubeIPv4 = '78.47.216.176', kubeIPv6 = '2a01:4f8:1c1c:6019::1';
+var IPv4 = '78.47.216.176', IPv6 = '2a01:4f8:1c1c:6019::1';
 
 D('iddqd.uk', NewRegistrar('none'), DnsProvider(NewDnsProvider('cloudflare')), DefaultTTL(1),
   // iddqd.uk (cloudflare pages)
@@ -13,16 +12,12 @@ D('iddqd.uk', NewRegistrar('none'), DnsProvider(NewDnsProvider('cloudflare')), D
   AAAA('www', '100::', CF_PROXY_ON),
 
   // kube.iddqd.uk
-  A('kube', kubeIPv4, TTL(86400)),
-  AAAA('kube', kubeIPv6, TTL(86400)),
+  A('kube', IPv4, TTL(86400)),
+  AAAA('kube', IPv6, TTL(86400)),
 
   // ww2.iddqd.uk (http(HTTP_PROXY_PORT)+tg(443) proxy)
-  A('ww2', kubeIPv4, TTL(86400)),
-  AAAA('ww2', kubeIPv6, TTL(86400)),
-
-  // ww1.iddqd.uk (http(2080)+tg(443) proxy) // TODO: remove
-  A('ww1', legacyIPv4, TTL(86400)),
-  AAAA('ww1', legacyIPv6, TTL(86400)),
+  A('ww2', IPv4, TTL(86400)),
+  AAAA('ww2', IPv6, TTL(86400)),
 
   // blog.iddqd.uk (github pages)
   CNAME('blog', 'tarampampam.github.io.', CF_PROXY_ON),
